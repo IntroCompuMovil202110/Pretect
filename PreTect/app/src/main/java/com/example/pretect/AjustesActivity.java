00202f6834d0e992917458f3cbf5c8278ad9c6bd
Daniel <danieltib99@gmail.com>
2021-02-28 23:28:39 -0500

    Implementacion mockups login, signup y ajustes

--- PreTect/app/src/main/java/com/example/pretect/AjustesActivity.java
+++ PreTect/app/src/main/java/com/example/pretect/AjustesActivity.java
@@ -3,12 +3,32 @@ package com.example.pretect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class AjustesActivity extends AppCompatActivity {
+
+    TextView phone, emergencyContact, safeWord, baitWord;
+    Button logout;
+    Switch recordSwitch, geolocationSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajustes);
+
+
+        //inflate
+        phone = findViewById(R.id.phone);
+        emergencyContact = findViewById(R.id.emergencyContact);
+        safeWord = findViewById(R.id.safeWord);
+        baitWord = findViewById(R.id.baitWord);
+        recordSwitch = findViewById(R.id.recordSwitch);
+        geolocationSwitch = findViewById(R.id.geolocationSwitch);
+        logout = findViewById(R.id.logout);
+
+
+
     }
 }
--- PreTect/app/src/main/java/com/example/pretect/SigninActivity.java
+++ PreTect/app/src/main/java/com/example/pretect/SigninActivity.java
@@ -2,13 +2,35 @@ package com.example.pretect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SigninActivity extends AppCompatActivity {
+
+    TextView emailTextFieldSI, passwordTextFieldSI;
+    Button signinButton, noAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        emailTextFieldSI = findViewById(R.id.emailTextFieldSI);
+        passwordTextFieldSI = findViewById(R.id.passwordTextFieldSI);
+        signinButton = findViewById(R.id.signupButton);
+        noAccountButton = findViewById(R.id.hasAccountButton);
+
+        //To Signip
+        noAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
