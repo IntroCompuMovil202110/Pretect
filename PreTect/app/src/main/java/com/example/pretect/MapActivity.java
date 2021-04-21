ce1ae1a2ecd464037782498836aa91599243b5c2
Reshi01 <danieldanih97@gmail.com>
2021-04-21 15:05:16 -0500

    map working

--- PreTect/app/src/main/java/com/example/pretect/MapActivity.java
+++ PreTect/app/src/main/java/com/example/pretect/MapActivity.java
@@ -1,33 +1,118 @@
 package com.example.pretect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.content.Intent;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.pretect.Utils.Functions;
-import com.example.pretect.entities.User;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
+import com.here.sdk.core.GeoCoordinates;
+import com.here.sdk.core.Location;
+import com.here.sdk.mapview.MapScheme;
+import com.here.sdk.mapview.MapView;
 
 public class MapActivity extends AppCompatActivity {
 
+    //General UI
     BottomNavigationView menuInferior;
+
+    //Permission
+    private String locationPerm = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final int LOCATION_PERMISSION_ID=1;
+    private boolean locationEnable=false;
+
+    //Map
+    private MapView mapView;
+    private Location myLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        //Menu Bar
         menuInferior = findViewById(R.id.bottom_nav_instructor);
         menuInferior.setOnNavigationItemSelectedListener(item -> {
             return Functions.navegacion(this, item);
         });
         ImageView mapPlaceHolder = new ImageView(getBaseContext());
-
         mapPlaceHolder.setImageResource(R.drawable.map_placeholder_background);
 
+        // Get a MapViewLite instance from the layout.
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        //Permissions
+        requestPermission(this, locationPerm,"Se necesita el permiso para mostrar los contactos",LOCATION_PERMISSION_ID);
+
+        loadMapScene();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    private void requestPermission(Activity context, String permission, String justification, int id){
+        if(ContextCompat.checkSelfPermission(context, permission)!= PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(context, permission)){
+                Toast.makeText(context, justification, Toast.LENGTH_SHORT).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permission}, id);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case LOCATION_PERMISSION_ID: {
+                if (requestCode == RESULT_OK) {
+                    loadMapScene();
+                } else {
+                    Toast.makeText(this,"Sin acceso a localización, hardware deshabilitado!",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+    private void loadMapScene(){
+        mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, mapError -> {
+            if(mapError == null){
+                mapView.getCamera().lookAt(new GeoCoordinates(52.5,13.3,1000));
+                initMyLocation();
+            }else{
+                //TODO: log error
+            }
+        });
+    }
+
+    private void initMyLocation() {
+        if(ContextCompat.checkSelfPermission(this, locationPerm)== PackageManager.PERMISSION_GRANTED) {
+            locationEnable=true;
+
+        }
     }
 }
