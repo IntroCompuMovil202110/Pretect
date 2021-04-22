cb3642326507ddf3d9ab173f6bd910646a21f215
Reshi01 <danieldanih97@gmail.com>
2021-04-22 12:54:02 -0500

    User Localization Working

--- PreTect/app/src/main/java/com/example/pretect/MapActivity.java
+++ PreTect/app/src/main/java/com/example/pretect/MapActivity.java
@@ -15,17 +15,24 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.pretect.Utils.Functions;
+import com.example.pretect.entities.PlatformPositioningProvider;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.here.sdk.core.Anchor2D;
 import com.here.sdk.core.GeoCoordinates;
-import com.here.sdk.core.Location;
 import com.here.sdk.mapview.MapError;
+import com.here.sdk.mapview.MapImage;
+import com.here.sdk.mapview.MapImageFactory;
+import com.here.sdk.mapview.MapMarker;
 import com.here.sdk.mapview.MapScene;
 import com.here.sdk.mapview.MapScheme;
 import com.here.sdk.mapview.MapView;
+
 
 public class MapActivity extends AppCompatActivity {
 
@@ -44,7 +51,16 @@ public class MapActivity extends AppCompatActivity {
 
     //Map
     private MapView mapView;
-    private Location myLocation;
+    private MapImage othersMapMarker;
+    private Button centerButton;
+
+    //My Location
+    private android.location.Location myLocation;
+    private PlatformPositioningProvider platformPositioningProvider = null;
+    private MapImage myMapImage;
+    private MapMarker myMarker=null;
+
+
 
     //Life Cycle
     @Override
@@ -52,13 +68,17 @@ public class MapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        //Inflate
+        centerButton= findViewById(R.id.centerButton);
+        centerButton.setVisibility(View.INVISIBLE);
+
         //Menu Bar
-//        menuInferior = findViewById(R.id.bottom_nav_instructor);
-//        menuInferior.setOnNavigationItemSelectedListener(item -> {
-//            return Functions.navegacion(this, item);
-//        });
-//        ImageView mapPlaceHolder = new ImageView(getBaseContext());
-//        mapPlaceHolder.setImageResource(R.drawable.map_placeholder_background);
+        menuInferior = findViewById(R.id.bottom_nav_instructor);
+        menuInferior.setOnNavigationItemSelectedListener(item -> {
+            return Functions.navegacion(this, item);
+        });
+        ImageView mapPlaceHolder = new ImageView(getBaseContext());
+        mapPlaceHolder.setImageResource(R.drawable.map_placeholder_background);
 
         //Light Sensor
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -70,7 +90,7 @@ public class MapActivity extends AppCompatActivity {
         mapView.onCreate(savedInstanceState);
 
         //Permissions
-        //requestPermission(this, locationPerm,"Se necesita el permiso para mostrar los contactos",LOCATION_PERMISSION_ID);
+        requestPermission(this, locationPerm,"Se necesita el permiso para acceder a la ubicacion",LOCATION_PERMISSION_ID);
 
         //Map
         loadMapScene();
@@ -81,6 +101,9 @@ public class MapActivity extends AppCompatActivity {
         super.onPause();
         mapView.onPause();
         sensorManager.unregisterListener(lightSensorListener);
+        if(locationEnable){
+            platformPositioningProvider.stopLocating();
+        }
     }
 
     @Override
@@ -88,6 +111,9 @@ public class MapActivity extends AppCompatActivity {
         super.onResume();
         mapView.onResume();
         sensorManager.registerListener(lightSensorListener, lightSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        if(locationEnable){
+            starLocating();
+        }
     }
 
     @Override
@@ -104,6 +130,8 @@ public class MapActivity extends AppCompatActivity {
                 Toast.makeText(context, justification, Toast.LENGTH_SHORT).show();
             }
             ActivityCompat.requestPermissions(context, new String[]{permission}, id);
+        }else{
+            initMyLocation();
         }
     }
 
@@ -111,7 +139,11 @@ public class MapActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == LOCATION_PERMISSION_ID) {
-            loadMapScene();
+            if(ContextCompat.checkSelfPermission(this, locationPerm)== PackageManager.PERMISSION_GRANTED) {
+                loadMapScene();
+                initMyLocation();
+
+            }
         }
     }
 
@@ -119,8 +151,9 @@ public class MapActivity extends AppCompatActivity {
     private void loadMapScene(){
         mapView.getMapScene().loadScene(MapScheme.NORMAL_DAY, mapError -> {
             if(mapError == null){
-                mapView.getCamera().lookAt(new GeoCoordinates(52.5,13.3,1000));
-                //initMyLocation();
+                if(!locationEnable){
+                    mapView.getCamera().lookAt(new GeoCoordinates(0,0,10000));
+                }
             }else{
                 //TODO: log error
             }
@@ -129,24 +162,47 @@ public class MapActivity extends AppCompatActivity {
 
     //My Location Updates
     private void initMyLocation() {
+        if(locationEnable)
+            return;
         if(ContextCompat.checkSelfPermission(this, locationPerm)== PackageManager.PERMISSION_GRANTED) {
             locationEnable=true;
-            mapView.getCamera().lookAt(new GeoCoordinates(52.5,13.3,1000));
-
+            myMapImage= MapImageFactory.fromResource(this.getResources(),R.drawable.my_marker);
+            platformPositioningProvider = new PlatformPositioningProvider(MapActivity.this);
+            centerButton.setVisibility(View.VISIBLE);
+            starLocating();
         }
+    }
+
+    private void starLocating() {
+        if(platformPositioningProvider==null){
+            //TODO: Handle error
+            return;
+        }
+        platformPositioningProvider.startLocating(new PlatformPositioningProvider.PlatformLocationListener() {
+            @Override
+            public void onLocationUpdated(android.location.Location location) {
+                myLocation=location;
+                if(myMarker==null){
+                    Anchor2D anchor2D=new Anchor2D(0.5f,1.0f);
+                    myMarker=new MapMarker(new GeoCoordinates(location.getLatitude(),location.getLongitude()),myMapImage,anchor2D);
+
+                }else{
+                    myMarker.setCoordinates(new GeoCoordinates(location.getLatitude(),location.getLongitude()));
+                }
+                mapView.getMapScene().addMapMarker(myMarker);
+                mapView.getCamera().lookAt(new GeoCoordinates(location.getLatitude(),location.getLongitude(),10000));
+            }
+        });
     }
 
     //Light Sensor
     private void initLightSensor() {
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        Toast.makeText(MapActivity.this, "Try", Toast.LENGTH_SHORT).show();
         if (lightSensor!=null){
-            Toast.makeText(MapActivity.this, "YES", Toast.LENGTH_SHORT).show();
             lightSensorListener = new SensorEventListener() {
                 @Override
                 public void onSensorChanged(SensorEvent event) {
-                    Toast.makeText(MapActivity.this, "CAMBIO", Toast.LENGTH_SHORT).show();
                     if (mapView != null) {
                         if (event.values[0] < 10000) {
                             Log.i("MAPS", "DARK MAP " + event.values[0]);
@@ -161,7 +217,6 @@ public class MapActivity extends AppCompatActivity {
                 public void onAccuracyChanged(Sensor sensor, int accuracy) {}
             };
         }
-        else Toast.makeText(MapActivity.this, "FAIL", Toast.LENGTH_SHORT).show();
     }
 
     private void changeScheme(MapScheme scheme) {
@@ -176,4 +231,13 @@ public class MapActivity extends AppCompatActivity {
             }
         });
     }
+
+
+    //Extra Services
+    public void centerMap(View view){
+        if(locationEnable){
+            mapView.getCamera().lookAt(new GeoCoordinates(myLocation.getLatitude(),myLocation.getLongitude(),10000));
+        }
+    }
+
 }
